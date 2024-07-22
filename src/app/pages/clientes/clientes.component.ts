5bbc747b7e4f348412d94e8ffc23b7f06443bd6e
Juan Antonio <musasime4@gmail.com>
2024-07-22 15:33:26 +0200

    feat: PMS-008_Rellenar_Clientes - Añadir titulo y clientes en cuadrícula

--- src/app/pages/clientes/clientes.component.ts
+++ src/app/pages/clientes/clientes.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
-
+import sliderClientes from '../../../../public/images/clientes/sliderClientes.json';
 @Component({
   selector: 'app-clientes',
   templateUrl: './clientes.component.html',
   styleUrl: './clientes.component.css'
 })
 export class ClientesComponent {
+  clientes: any[] = sliderClientes
 
+  tituloInicial = {
+    "tipo": "Clientes",
+    "titulo": "Nuestros Clientes"
+  }
 }
